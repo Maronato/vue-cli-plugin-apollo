ad34a6ba27458657a9d35820147ff7195b8692c3
Maronato <gustavomaronato@gmail.com>
2020-11-21 02:11:11 -0300

    fix lint

--- client-addon/.eslintrc.js
+++ client-addon/.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     '@vue/standard',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warning' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'comma-dangle': ['error', 'always-multiline'],
   },
